744d328d0139f3787884e1632774cdd36454bf40
1bleh7 <sochima.obiefule@lorma.edu>
2026-09-10 10:29:27 +0800

    Lab 4: Server-Rendered CRUD Interface

---
+++ week4/src/main/java/com/zilpher/webdev2/Book.java
@@ -0,0 +1,32 @@
+package com.zilpher.webdev2;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }public String getTitle() {
+        return title;
+    }public String getAuthor() {
+        return author;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }public void setTitle(String title) {
+        this.title = title;
+    }public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ week4/src/main/java/com/zilpher/webdev2/Category.java
@@ -0,0 +1,17 @@
+package com.zilpher.webdev2;
+
+public class Category {
+    private String name;
+
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }public void setName(String name) {
+        this.name = name;
+    }
+}
